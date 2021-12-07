96df8bf4e5c25aae5d8dd24e56d67fee7d37bb28
slothexpress <hellosami@protonmail.com>
2021-12-07 21:48:21 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include "raylib.h"
+#include <string>
+
+int main()
+{
+    const int windowWidth = 512;
+    const int windowHeight = 380;
+    const int jumpVelocity = -800;
+    const int gravity = 3000;
+    const float updateTime = 1.0 / 12.00;
+    float runningTime = 0.0;
+    int playerVelocity = 0;
+    int frame = 0;
+
+    // Open window
+    InitWindow(windowWidth, windowHeight, "Samiii");
+
+    Texture2D nebula = LoadTexture("textures/nebula.png");
+    Rectangle nebulaRectangle{0.0, 0.0, nebula.width/8, nebula.height/8};
+    Vector2 nebulaPosition{windowWidth, windowHeight - nebulaRectangle.height};
+
+    int nebulaVelocity = -600;
+
+    Texture2D player = LoadTexture("textures/scarfy.png");
+    Rectangle playerRectangle{0.0, 0.0, player.width/6, player.height};
+    Vector2 playerPosition{windowWidth/2 - (playerRectangle.width/2), windowHeight - playerRectangle.height};
+
+    // Frames per second
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
+    {
+        bool jump = IsKeyPressed(KEY_SPACE);
+        bool onTheGround = playerPosition.y >= (windowHeight - playerRectangle.height);
+        const float timeSinceLastFrame = GetFrameTime();
+        
+        BeginDrawing();
+        ClearBackground(WHITE);  
+
+        // Add gravity if NOT on the ground
+        playerVelocity = onTheGround ? 0 : playerVelocity + (gravity * timeSinceLastFrame);
+
+        if(jump && onTheGround)
+        {
+            playerVelocity = playerVelocity + jumpVelocity;
+        }
+
+        playerPosition.y = playerPosition.y + (playerVelocity * timeSinceLastFrame);
+
+        runningTime = runningTime + timeSinceLastFrame;
+        if(runningTime >= updateTime)
+        {
+            runningTime = 0.0;
+            playerRectangle.x = frame * playerRectangle.width;
+            frame++;
+            if(frame > 5)
+            {
+                frame = 0;
+            }
+        }
+
+        DrawTextureRec(nebula, nebulaRectangle, nebulaPosition, WHITE);
+        DrawTextureRec(player, playerRectangle, playerPosition, WHITE);
+
+        EndDrawing();
+        
+    }
+    UnloadTexture(player);
+    UnloadTexture(nebula);
+    CloseWindow();
+}
