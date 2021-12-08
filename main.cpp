1ca7f1b2b350a6de704c8a93bafb5b3bdcf47824
slothexpress <hellosami@protonmail.com>
2021-12-08 17:43:21 +0100

    Add first obstacle/nebula with movement

--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ int main()
     const int windowWidth = 512;
     const int windowHeight = 380;
     const int jumpVelocity = -800;
+    const int nebulaVelocity = -300;
     const int gravity = 3000;
     const float updateTime = 1.0 / 12.00;
     float runningTime = 0.0;
-    int playerVelocity = 0;
+    int playerVelocity = 0;    
+   
     int frame = 0;
 
     // Open window
@@ -17,9 +19,7 @@ int main()
 
     Texture2D nebula = LoadTexture("textures/nebula.png");
     Rectangle nebulaRectangle{0.0, 0.0, nebula.width/8, nebula.height/8};
-    Vector2 nebulaPosition{windowWidth, windowHeight - nebulaRectangle.height};
-
-    int nebulaVelocity = -600;
+    Vector2 nebulaPosition{windowWidth-40, windowHeight - nebulaRectangle.height};
 
     Texture2D player = LoadTexture("textures/scarfy.png");
     Rectangle playerRectangle{0.0, 0.0, player.width/6, player.height};
@@ -45,23 +45,31 @@ int main()
             playerVelocity = playerVelocity + jumpVelocity;
         }
 
+        if(frame > 5)
+        {
+            nebulaPosition.x = nebulaPosition.x + (nebulaVelocity * timeSinceLastFrame);
+        }
+        
         playerPosition.y = playerPosition.y + (playerVelocity * timeSinceLastFrame);
 
         runningTime = runningTime + timeSinceLastFrame;
-        if(runningTime >= updateTime)
+
+        if(onTheGround)
         {
-            runningTime = 0.0;
-            playerRectangle.x = frame * playerRectangle.width;
-            frame++;
-            if(frame > 5)
+            if(runningTime >= updateTime)
             {
-                frame = 0;
+                runningTime = 0.0;
+                playerRectangle.x = frame * playerRectangle.width;
+                frame++;
+                if(frame > 5)
+                {
+                    frame = 0;
+                }
             }
         }
 
         DrawTextureRec(nebula, nebulaRectangle, nebulaPosition, WHITE);
         DrawTextureRec(player, playerRectangle, playerPosition, WHITE);
-
         EndDrawing();
         
     }
